a9a2cb13916c8b82d6bcdfb8b6835d666a2339c0
over0219 <over0219@d.umn.edu>
2017-02-21 15:05:48 -0600

    minor change

--- cmake/IncludeEigen3.cmake
+++ cmake/IncludeEigen3.cmake
@@ -22,9 +22,9 @@
 find_package(Eigen3)
 if(NOT EIGEN3_FOUND)
 	set(EIGEN3_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/eigen3 )
-	message("Eigen3 not found, cloning into ${EIGEN3_DIR}")
 	# We'll just use OMP's Eigen3 for now
 	IF(NOT EXISTS ${EIGEN3_DIR})
+		message("Eigen3 not found, cloning into ${EIGEN3_DIR}")
 #		execute_process(COMMAND git clone https://github.com/OPM/eigen3 ${EIGEN3_DIR})
 		execute_process(COMMAND hg clone https://bitbucket.org/eigen/eigen/ ${EIGEN3_DIR})
 	endif()
