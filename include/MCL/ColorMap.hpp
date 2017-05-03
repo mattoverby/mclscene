f160b8e0bd7a81cbc63cc1890e213d7afdfeca2a
over0219 <over0219@d.umn.edu>
2017-05-03 15:04:45 -0500

    updated colormap

--- include/MCL/ColorMap.hpp
+++ include/MCL/ColorMap.hpp
@@ -16,8 +16,6 @@
 // OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
 // IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// By Matt Overby (http://www.mattoverby.net)
 
 #ifndef MCLSCENE_COLORMAP_H
 #define MCLSCENE_COLORMAP_H 1
@@ -32,36 +30,45 @@ namespace mcl {
 //
 class ColorMap {
 public:
-	// Preset ramps
+	// Preset ramps: mapping goes from
+	// 0 = first color, 1 = last color
 	enum {
-		GRAYSCALE,
-		COLD_HOT, // blue -> red
-		BLACKBODY, // black -> yellow
+		GRAYSCALE, // grayscale
+		COLD_HOT, // blue to red
+		BLACKBODY, // black, yellow, white
 	};
 
-	ColorMap(){ use_preset(GRAYSCALE); }
+	// "gradlist" stores a list of loaded color gradients.
+	// "colors" is a pointer to the activly loaded color gradient.
+	ColorMap(){
+		gradlist[ "mcl_preset_ramp" ] = std::vector<cpair>();
+		colors = &gradlist[ "mcl_preset_ramp" ];
+		use_preset(GRAYSCALE);
+	}
 
 	// Value is between 0 and 1
 	// The avg value is used for 3-color gradients, with the
 	// middle color being the average.
 	mcl::Vec3f get( float value );
 
-	// Adds a color to the ramp at position (0-1) val
+	// Adds a color to the current ramp at position (0-1) val
 	void add( float val, const mcl::Vec3f &c );
 
-	// Load a preset map
-	void use_preset( int preset );
-
-	// Loads a paraview xml file, which can later be 
-	// used with the "use(...)" command.
-	// See http://www.paraview.org/Wiki/Colormaps for details.
-	void load_paraview( std::string xmlfile ){}
-
-	// Loads a previously loaded color map (excluding preset)
-	void use( std::string label ){}
+	// Load a preset map. The "avg" argument is used
+	// to set the middle of the ramp which is only used
+	// for some gradients (e.g. hot cold).
+	void use_preset( int preset, float avg=0.5 );
 
 	// Clears current gradient
-	void clear(){ colors.clear(); }
+	void clear(){ colors->clear(); }
+
+	// Use a labeled color gradient loaded with
+	// the "load_" function. Returns true if the gradient
+	// was successfully loaded.
+//	bool use( std::string label ){}
+
+	// Load a ParaView colormap xml file.
+//	void load_paraview( std::string xml ){}
 
 	// Javascript hex colors from colorbrewer2.org, e.g.:
 	// mymap.set("['#e5f5f9','#99d8c9','#2ca25f']");
@@ -72,13 +79,8 @@ private:
 		cpair( float v_, mcl::Vec3f c_ ){ c=c_; v=v_; }
 		float v; mcl::Vec3f c;	
 	};
-	std::vector<cpair> colors; // the current color map
-
-	// You can load color map files like paraview, which are
-	// stored here and index by their name.
-	std::unordered_map< std::string, std::vector<cpair> > loaded_maps;
-
-//	Vec3f hex_to_rgb( std::string hex_str );
+	std::vector<cpair> *colors; // currently loaded color map
+	std::unordered_map<std::string, std::vector<cpair> > gradlist;
 
 }; // end class color map
 
@@ -89,19 +91,19 @@ private:
 
 mcl::Vec3f ColorMap::get( float value ){
 
-	if( colors.size()==0 ){ use_preset(GRAYSCALE); }
+	if( colors->size()==0 ){ use_preset(GRAYSCALE); }
 
 	// Check min/max
-	if( value >= 1.f ){ return colors.back().c; }
-	else if( value <= 0.f ){ return colors.front().c; }
+	if( value >= 1.f ){ return colors->back().c; }
+	else if( value <= 0.f ){ return colors->front().c; }
 
 	// Loop gradient and find color point
-	const int n_c = colors.size();
+	const int n_c = colors->size();
 	for( int i=0; i<n_c; ++i ){
-		cpair *c = &colors[i];
+		cpair *c = &(*colors)[i];
 		if( value < c->v ){
 			// From http://www.andrewnoske.com
-			cpair *prevc = &colors[std::max(0,i-1)];
+			cpair *prevc = &(*colors)[std::max(0,i-1)];
 			float diff = prevc->v - c->v;
 			float frac = ( diff==0.f ) ? 0.f : (value-c->v) / diff;
 			mcl::Vec3f color = (prevc->c - c->c)*frac + c->c;
@@ -109,25 +111,28 @@ mcl::Vec3f ColorMap::get( float value ){
 		}
 	}
 
-	return colors.back().c; // return last color
+	return colors->back().c; // return last color
 
 } // end get color
 
 
 void ColorMap::add( float val, const mcl::Vec3f &c ){
 
+	if( val > 1.f ){ val = 1.f; }
+	if( val < 0.f ){ val = 0.f; }
+
 	// Add and sort with lambda. Doesn't work well with many inserts
-	colors.push_back( cpair(val,c) );
-	std::sort(colors.begin(), colors.end(), []( const cpair &left, cpair &right) {
+	colors->push_back( cpair(val,c) );
+	std::sort(colors->begin(), colors->end(), []( const cpair &left, cpair &right) {
 		return left.v < right.v;
 	});
+}
 
-} // end add color
 
+void ColorMap::use_preset( int preset, float avg ){
 
-void ColorMap::use_preset( int preset ){
-
-	colors.clear();
+	// We'll just keep overwriting the same preset
+	colors->clear();
 	switch (preset) {
 
 		default:{
@@ -142,11 +147,14 @@ void ColorMap::use_preset( int preset ){
 		} break;
 
 		case COLD_HOT:{
-			add( 0.f, Vec3f(0,1,1) );
-			add( 0.45f, Vec3f(0,0,1) );
-			add( 0.5f, Vec3f(1,1,1) );
-			add( 0.55f, Vec3f(1,0,0) );
-			add( 1.f, Vec3f(1,1,0) );
+			float frac = std::min( (1.f-avg), avg )*0.2f;
+			float upper = avg+frac;
+			float lower = avg-frac;
+			add( 1.f, Vec3f(202,0,32) );
+			add( upper, Vec3f(244,165,130) );
+			add( lower, Vec3f(146,197,222) );
+			add( 0.f, Vec3f(5,113,176) );
+			for( int i=0; i<colors->size(); ++i ){ (*colors)[i].c *= 1.f/255.f; }
 		} break;
 
 		case BLACKBODY:{
